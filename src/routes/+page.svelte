cfd79dfb6b672a1bf6d509b0f57b48aee75a04db
Garik Asplund <garik.asplund@gmail.com>
2023-11-04 22:01:03 -0700

    svelty picker first push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import SveltyPicker, { formatDate } from 'svelty-picker';
     import { en } from 'svelty-picker/i18n';
-
+    // import { supabase } from '$lib/supabaseClient';
 
     const endDate = new Date();
     endDate.setFullYear(endDate.getFullYear() + 1);
@@ -21,6 +21,23 @@
     export let data;
     let blocked = [...data.countries];
     console.log(blocked)
+    let value;
+    function handleSubmit() {
+        // Convert dates to ISO strings before inserting into the database
+        const formattedStartDate = String(value).split(',')[0];
+        const formattedEndDate = String(value).split(',')[1];
+
+        console.log(formattedStartDate, formattedEndDate);
+
+        // Insert dates into your Supabase table
+        supabase.from('countries').insert([
+            { start_date: formattedStartDate, end_date: formattedEndDate }
+        ]).then(response => {
+            console.log('Dates inserted successfully:', response.data);
+        }).catch(error => {
+            console.error('Error inserting dates:', error);
+        });
+    }
 </script>
 
 <ul>
@@ -28,12 +45,20 @@
         <li>{country.name}</li>
     {/each}
 </ul>
-<SveltyPicker 
+<form on:submit|preventDefault={handleSubmit}>
+    <SveltyPicker 
     isRange 
+    pickerOnly
     startDate={String(new Date())}
     endDate={endDate} 
     todayBtn={false} 
     disableDatesFn={disableWeekends}
     autocommit={false}
-
-/>
+    displayFormat="dd M yyyy"
+    bind:value
+    />
+    <br>
+    <button style="width: 33rem; height: 2rem; margin: 1rem" type="submit">Submit dates</button>
+</form>
+<br>
+{value}
